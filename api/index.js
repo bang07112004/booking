dae9234a8324caaced2c2b59445b8296c232b853
bang07112004 <phong0909191145@gmail.com>
2023-02-02 12:39:40 +0700

    check

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173/",
   })
 );
 mongoose.set("strictQuery", true);
